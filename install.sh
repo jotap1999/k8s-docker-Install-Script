2f3a506193dfc2ac7cc5e64fa2d9e05453fc9822
José Franco <54073559+jotap1999@users.noreply.github.com>
2020-09-01 12:16:22 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,44 @@
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+cat >> /etc/sysctl.conf  <<EOF
+net.ipv4.ip_forward=1
+net.bridge.bridge-nf-call-arptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl  -p
+
+mkdir -p /etc/systemd/system/docker.service.d
+systemctl daemon-reload
+systemctl enable docker
+systemctl restart docker
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+apt -y install kubeadm kubelet kubectl
+systemctl enable kubelet
